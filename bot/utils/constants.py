df3f210379df2a8e71e54a14486539c6943e2333
Anand Krishna <anandkrishna2312@gmail.com>
2021-05-08 21:08:10 +0400

    backend: make constants

---
+++ bot/utils/constants.py
@@ -0,0 +1,12 @@
+"""Some constants used in the project."""
+from enum import Enum
+
+
+class EmbedColour(Enum):
+    """
+    Colors used throughout embeds returned by the bot.
+    """
+
+    Error = 0xFF0000
+    Info = 0xF4C2C2
+    Success = 0x39FF14
