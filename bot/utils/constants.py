815cda66d5bfb05bede8f55299340388c27a542c
Anand Krishna <40204976+anand2312@users.noreply.github.com>
2021-06-03 16:22:04 +0400

    Add emoji constants

--- bot/utils/constants.py
+++ bot/utils/constants.py
@@ -10,3 +10,12 @@ class EmbedColour(Enum):
     Error = 0xFF0000
     Info = 0xF4C2C2
     Success = 0x39FF14
+    
+class VoteEmoji(Enum):
+    """
+    Emojis used for voting and suggestions.
+    """
+    
+    Check = "<:check:849985604993548308>"
+    Neutral = "<:neutral:849985545333243944>"
+    Cross = "<:cross:849985759750389780>"
