8ea2a16f0a9857def83c6e9a1ae13f0daeac8ac1
Anand Krishna <40204976+anand2312@users.noreply.github.com>
2021-06-03 16:32:00 +0400

    Add get_suggestion_channel function

--- bot/backend/suggestions.py
+++ bot/backend/suggestions.py
@@ -11,9 +11,18 @@ async def send_to_suggestions(channel: discord.TextChannel, *, embed: discord.Em
     m = await channel.send(embed=embed)
     for e in VoteEmoji:
         await m.add_reaction(e.value)
-        
+      
+    
 def build_embed(suggestion: str, author: discord.Member) -> discord.Embed:
     e = discord.Embed(description=suggestion, color=EmbedColour.Info.value)
     e.set_author(name=author.display_name, icon_url=author.avatar_url)
     e.timestamp = datetime.datetime.utcnow()
     return e
+
+
+async def get_guild_suggestions_channel(bot: discord.ext.commands.Bot, guild: int) -> discord.TextChannel:
+    """
+    Gets the guild's suggestions channnel.
+    """
+    # implement this once db is set up
+    return bot.get_channel(849987442089787442)
