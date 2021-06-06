6bfa4b0543d6dee0b0b8b544e0b9a38d1568aa27
Anand Krishna <40204976+anand2312@users.noreply.github.com>
2021-06-06 17:52:26 +0400

    fix: stupid

--- bot/utils/converters.py
+++ bot/utils/converters.py
@@ -11,7 +11,7 @@ FORMATTED_CODE_REGEX = re.compile(
 )
 
 
-class CodeBlockConverter(Converter):
+class CodeBlockConverter(commands.Converter):
     async def convert(self, ctx: commands.Context, code: str):
         """
         Returns a codeblock from the message.
