3e4c56ff65fb8b352a445a8c3f5ef951e8b34c12
Anand Krishna <40204976+anand2312@users.noreply.github.com>
2021-06-08 15:01:14 +0400

    feat: allow re-eval on message edit

--- bot/commands/eval.py
+++ bot/commands/eval.py
@@ -1,9 +1,10 @@
 import asyncio
+from datetime import datetime
 from io import StringIO
-from typing import Optional
+from typing import Mapping, Optional
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 from bot.utils.converters import CodeblockConverter
 
@@ -14,7 +15,8 @@ PISTON_API_URL = "https://emkc.org/api/v1/piston/execute"
 class EvalListener(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
-        
+        self._eval_messages: Mapping[discord.Message, discord.Message] = {}  # dict of code message to bot's reply
+     
     def prepare_file_output(self, content: str, language: str) -> discord.File:
         """
         Put outputs in File objects that will be rendered by Discord.
@@ -27,14 +29,14 @@ class EvalListener(commands.Cog):
         buffer = StringIO(content)
         return discord.File(buffer, filename=f"output.{lang_to_extension.get(language, 'txt')}")
         
-    async def wait_for_response(self, message: discord.Message) -> bool:
+    async def wait_for_response(self, message: discord.Message, emoji: str = "▶") -> bool:
         """
         Waits 30 seconds for the user to respond to evaluate the codeblock.
         """
-        await message.add_reaction("▶")
+        await message.add_reaction(emoji)
         
         def check(rxn: discord.Reaction, user: discord.User) -> bool:
-            return str(rxn.emoji) == "▶" and user == message.author
+            return str(rxn.emoji) == emoji and user == message.author
         
         try:
             _, _ = await self.bot.wait_for("reaction_add", check=check, timeout=30)
@@ -51,6 +53,10 @@ class EvalListener(commands.Cog):
         
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> Optional[discord.Message]:
+        """
+        Add a playbutton reaction to messages with codeblocks, to evaluate the code and
+        send the output in chat.
+        """
         ctx = await self.bot.get_context(message)
         match, code = await CodeblockConverter().convert(ctx, message.content)
         
@@ -78,7 +84,63 @@ class EvalListener(commands.Cog):
         lines = output.splitlines()
         
         file = self.prepare_file_output(output, language)
-        return await ctx.reply(file=file)
+        reply = await ctx.reply(file=file)
+        
+        # add message to cache
+        self._eval_messages[message] = reply
+        
+    @commands.Cog.listener()
+    async def on_message_edit(self, before: discord.Message, after: discord.Message) -> None:
+        """
+        Re-evaluate code if they are edited.
+        """
+        bot_reply = self._eval_messages.get(before)
+        
+        if not bot_reply:  # not a code eval block, ignore
+            return
+        
+        ctx = await self.bot.get_context(message)
+        match, code = await CodeblockConverter().convert(ctx, message.content)
+        language = match.group("lang")
+        
+        to_eval = await self.wait_for_response(message, emoji="🔁")
+        
+        if not to_eval:
+            return
+        
+        eval_data = await self._run_eval(ctx, language, code)
+        
+        error_msg = eval_data.get("msg")
+        if error_msg:
+            return await ctx.reply(
+                embed=discord.Embed(
+                    title="Something went wrong.", description=error_msg, color=discord.Colour.red()
+                )
+            )
+        
+        if eval_data["language"] in ("python2", "python3"):
+            eval_data["language"] = "python"
+            
+        output = eval_data["output"].strip().replace("```", '`\u200b``')
+        lines = output.splitlines()
+        
+        file = self.prepare_file_output(output, language)
+                                               
+        await bot_reply.delete()
+        reply = await ctx.reply(file=file)
+        
+    @tasks.loop(minutes=1)
+    async def clear_stale_eval_messages(self) -> None:
+        """
+        Go through the cache and remove the old messages.
+        """
+        now = datetime.utcnow()
+        
+        for message in self._eval_messages:
+            delta = now - message.created_at
+            
+            if delta.seconds >= 600:
+                del self._eval_messages[message]
 
 
 def setup(bot: commands.Bot) -> None:
