5017c92888d9c09f3bb209d528ef9faa494a48b9
anand2312 <anandkrishna2312@gmail.com>
2021-07-20 21:45:41 +0400

    feat: add reaction_menu to context

--- bot/internal/context.py
+++ bot/internal/context.py
@@ -1,6 +1,13 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator, List, Optional, Union, TYPE_CHECKING
+
+import discord
 from discord.ext import commands
 
 from bot.backend.models import User
+
+if TYPE_CHECKING:
+    from bot.internal.bot import UtilityBot
 
 
 class UtilityContext(commands.Context):
@@ -11,3 +18,56 @@ class UtilityContext(commands.Context):
     @property
     def db_user(self) -> User:
         return User(id=self.author.id)  # type: ignore ; dpy weird
+
+    @asynccontextmanager
+    async def reaction_menu(
+        self,
+        *,
+        message: Optional[discord.Message] = None,
+        emojis: List[Union[str, discord.Emoji]],
+        wait: int = 60,
+        prompt: discord.Embed
+    ) -> AsyncIterator[discord.Reaction]:
+        """
+        Starts a reaction menu. The context manager will automatically clear reactions.
+        Args:
+            All arguments are keyword-only.
+            message: The message object that will be edited to `prompt`. If None, a new message is sent.
+            prompt: Embed containing the prompt question.
+            emojis: Set of emojis that will be added as reactions.
+            wait: seconds to wait before clearing the reactions. Defaults to 60.
+        Yields:
+            discord.Reaction
+        The context manager will automatically clear reactions.
+        """
+        if message:
+            await message.edit(embed=prompt)
+        else:
+            message: discord.Message = await self.send(embed=prompt)
+
+        emoji_set = set()
+        for emoji in emojis:
+            await message.add_reaction(emoji)
+            emoji_set.add(str(emoji))
+
+        def check(
+            reaction: discord.Reaction, user: Union[discord.Member, discord.User]
+        ) -> bool:
+            return (
+                reaction.message == message
+                and str(reaction) in emoji_set
+                and user == self.author
+            )
+
+        self.bot: UtilityBot  # to quite pylance down in the next `wait_for`
+
+        try:
+            reaction, _ = await self.bot.wait_for(
+                "reaction_add", check=check, timeout=wait
+            )
+            yield reaction
+        finally:
+            if not isinstance(
+                message.channel, discord.DMChannel
+            ):  # clearing reactions in DMs raises Forbidden
+                await message.clear_reactions()
